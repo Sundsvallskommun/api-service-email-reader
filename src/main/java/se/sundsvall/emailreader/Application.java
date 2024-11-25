6ebed8059cd558b8e512da1cdf3874da1629d30d
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-11-25 09:09:58 +0100

    Fix Shedlock bean load issue

--- src/main/java/se/sundsvall/emailreader/Application.java
+++ src/main/java/se/sundsvall/emailreader/Application.java
@@ -4,14 +4,11 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import se.sundsvall.dept44.ServiceApplication;
 
 @ServiceApplication
 @EnableFeignClients
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "${scheduled.shedlock-lock-at-most-for}")
 public class Application {
 	public static void main(final String... args) {
 		run(Application.class, args);
