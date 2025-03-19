a2af2d8fc493c336636580699279482a34176a34
cheezi0747 <linus.sjolinder@b3.se>
2025-03-19 09:26:33 +0100

    UF-15045: Changes after review feedback

--- src/main/java/se/sundsvall/emailreader/integration/db/GraphCredentialsRepository.java
+++ src/main/java/se/sundsvall/emailreader/integration/db/GraphCredentialsRepository.java
@@ -4,17 +4,15 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import se.sundsvall.emailreader.integration.db.entity.CredentialsEntity;
+import se.sundsvall.emailreader.integration.db.entity.GraphCredentialsEntity;
 
 @CircuitBreaker(name = "graphCredentialsRepository")
-public interface GraphCredentialsRepository extends JpaRepository<CredentialsEntity, String> {
+public interface GraphCredentialsRepository extends JpaRepository<GraphCredentialsEntity, String> {
 
-	List<CredentialsEntity> findAllByMunicipalityId(final String municipalityId);
+	List<GraphCredentialsEntity> findAllByMunicipalityId(final String municipalityId);
 
 	void deleteByMunicipalityIdAndId(final String municipalityId, final String id);
 
-	Optional<CredentialsEntity> findByMunicipalityIdAndId(final String municipalityId, final String id);
-
-	List<CredentialsEntity> findAllByAction(final String action);
+	Optional<GraphCredentialsEntity> findByMunicipalityIdAndId(final String municipalityId, final String id);
 
 }
